11300c86f46ef29bcffc6c797e4777c8ce209910
Laubert <laubert.f@gmail.com>
2024-04-19 11:41:40 +0200

    on processor as ground truth

--- steplib.py
+++ steplib.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def apply_filter(data_buffer, coefficients):
@@ -8,7 +9,6 @@ def apply_filter(data_buffer, coefficients):
 
     # Create an array to hold the filtered data
     filtered_data = np.zeros_like(data_buffer)
-
 
     # Apply the filter
     for i in range(len(coefficients), len(data_buffer)):
@@ -84,4 +84,18 @@ def post_processing(signal, window_length_ms, sample_rate):
             i += 1
     return output
 
+
+def getSteps(file, window_size=20, threshold=1.15):
+    file_data = pd.read_csv("Sheep/" + file)
+    expected = np.where(file_data["gyrx"] != 0)[0]
+    data1 = file_data["accx"] ** 2 + file_data["accy"] ** 2 + file_data["accz"] ** 2
+    accelerometer = data1.apply(lambda x: x ** 0.5)
+    data2 = apply_filter(accelerometer, [0.0167855014066363, 0.0470217363830679, 0.121859539593060, 0.198782647391950,
+                                         0.231101150450572, 0.198782647391950, 0.121859539593060, 0.0470217363830679,
+                                         0.0167855014066363])
+    data1 = maximum_difference(data2, window_size)
+    data1 = detect_outliers(data1, threshold)
+    data1 = post_processing(data1, 250, 100)
+    calculated = np.where(data1 == 1)[0]
+    return calculated, expected, accelerometer
 # %%
